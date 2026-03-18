9980fdf4fc4dd17a07e554b28806c8e961bc7ad9
PavanSakthivel <sakpavan@gmail.com>
2026-03-18 09:18:22 +0530

    UC2 edited

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class UseCase2TrainConsistMgmnt {
+public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
